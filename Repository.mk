9f27de9ac9620f7286920cfe042cdcc414d8a610
Markus Mohrhard <markus.mohrhard@collabora.co.uk>
2015-01-28 16:19:10 +0100

    Revert "Revert "Revert "Revert "wrong merge conflict resolution, vcldemo, icontest go to instdir""""
    This reverts commit bf9435bce063da95deb8a52371bce04e7435c966.

--- Repository.mk
+++ Repository.mk
@@ -68,9 +68,6 @@ $(eval $(call gb_Helper_register_executables,NONE, \
         svpclient \
         pixelctl ) \
 	$(if $(and $(ENABLE_GTK), $(filter LINUX,$(OS))), tilebench) \
-	$(if $(filter LINUX MACOSX WNT,$(OS)),icontest \
-	    outdevgrind) \
-	vcldemo \
 	tiledrendering \
     mtfdemo \
 	$(if $(and $(ENABLE_GTK), $(filter LINUX,$(OS))), gtktiledviewer) \
@@ -151,6 +148,9 @@ $(eval $(call gb_Helper_register_executables_for_install,OOO,ooo, \
 	$(if $(filter WNT,$(OS)), \
 		senddoc \
 	) \
+	$(if $(filter LINUX MACOSX WNT,$(OS)),icontest \
+	    outdevgrind) \
+	vcldemo \
 ))
 
 ifeq ($(OS),WNT)
