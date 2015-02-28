7b3db5c939bab2b10f29a1899271021972677118
Michael Meeks <michael.meeks@collabora.com>
2015-02-28 19:59:01 +0000

    Double dispose protection.
    Change-Id: Iadc0a4fcb711e4f846e0e047880e512c9d42d0f8

--- sw/source/core/view/viewsh.cxx
+++ sw/source/core/view/viewsh.cxx
@@ -2329,7 +2329,7 @@ SwViewShell::CreateAccessiblePreview()
 
 void SwViewShell::InvalidateAccessibleFocus()
 {
-    if( Imp()->IsAccessible() )
+    if( Imp() && Imp()->IsAccessible() )
         Imp()->GetAccessibleMap().InvalidateFocus();
 }
 
